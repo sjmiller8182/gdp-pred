5b7af8e7e7c7b19434e73a90c5d658a924e9d4c7
Stuart Miller <Stuart.j.miller.8182@gmail.com>
2020-03-05 21:33:26 -0600

    Update build script Use file packing list rather than globbing

--- make.R
+++ make.R
@@ -1,15 +1,27 @@
 
 # import the rendering lib
 library(rmarkdown)
+library(yaml)
+library(git2r)
 
-# find all the Rmd files
-files.rmd <- Sys.glob('./analysis/*.Rmd')
+# read in packing list
+# and build settings
+cfg <- yaml::read_yaml('./_build_config.yml')
 
-print(files.rmd)
+cat('\nBuilding')
 
 # render each Rmd file
-for (f in files.rmd){
+for (f in cfg$packing_list){
+  cat(paste('\nBuilding', f))
   rmarkdown::render(f,
-                    output_format = 'html_document',
-                    output_dir = './docs')
+                    output_format = cfg$build_settings$format,
+                    output_dir = cfg$build_settings$output_dir,
+                    quiet = TRUE)
 }
+
+cat(paste('\nPushing new build to remote'))
+
+# add and commit the new build
+git2r::add(path = cfg$build_settings$output_dir)
+git2r::commit(message = paste('New build of docs', Sys.Date()))
+git2r::push()
